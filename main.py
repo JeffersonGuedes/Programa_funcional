c9aec60c8769a77548e8880205d92675b3472f6b
jefferson guedes <jefferson.guedes@sepog.fortaleza.ce.gov.br>
2025-03-19 14:10:38 -0300

    :snake: Feat: Criando novas funções para aumentar o escopo do projeto e aplicando closure e alta ordem

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 from functools import reduce
 
-# Sistema de Web Scrapper e uso de programação funcional
-# Estamos usando a biblioteca requests para fazer requisições get e BeautifulSoup para trazer informações da requisição para nosso código
-# Site que estamos fazendo requisição para trazer o valores da cotação de moeda
+# Sistema de Web Scrapper e uso de programação funcional.
+# Estamos usando a biblioteca requests para fazer requisições get e BeautifulSoup para trazer informações da requisição para nosso código.
+# Site que estamos fazendo requisição para trazer o valores da cotação de moeda.
 
 # exemplo de moedas para cotação KRW, INR, JPY, CAD, EUR, GBP, CNY, MXN, USD, BRL ...
 coin = {"krw", "inr", "jpy", "cad", "eur", "gbp", "cny", "mxn", "usd", "brl"}
 
-# validando informações recebidas
-# Valida se foi preenchido corretamente todos os campos, valida se o tipos estão de acordos se o moeda de origem e de destino estão de acordo com o conjunto e verifica se as moedas são divergente para poder fazer o calculo de conversão
+# validando informações recebidas.
+# Valida se foi preenchido corretamente todos os campos, valida se o tipos estão de acordos se o moeda de origem e de destino estão de acordo com o conjunto e verifica se as moedas são divergente para poder fazer o calculo de conversão.
 
 def validation(currency_quote, exchange, price):
     if not currency_quote or not exchange or not price:
@@ -23,7 +23,7 @@ def validation(currency_quote, exchange, price):
         print(f"Moeda de origem digitada inválida, {', '.join(coin)}")
         return client()
     if exchange not in coin:
-        print(f"Moeda de destino digitada inválida, {''.join(coin)}")
+        print(f"Moeda de destino digitada inválida, {', '.join(coin)}")
         return client()
     if exchange == currency_quote:
         print(f"Moeda de destino e origem não pode ser iguais")
@@ -31,30 +31,34 @@ def validation(currency_quote, exchange, price):
     print("deu bom")
     return currency_quote, exchange, price
 
+# requisição get para site de cotação ainda em desenvolvimento.
+def currency_converter(currency_quote, exchange):
+    def get_value(price):
+        #configurar o headers para requisição get do requests.
+        headers ={"user-agent" : "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Mobile Safari/537.36"}
+        link = f"https://wise.com/br/currency-converter/{currency_quote}-to-{exchange}-rate?amount={price}"
+        requisicao = requests.get(link, headers=headers)
+        return requisicao
+    return get_value
+
+# O cliente chamando a requisição especificando as inputs a seguir para personalizar a requests.
 def client():
     currency_quote = input("Qual moeda para cotação?").lower()
     exchange = input("Qual a moeda de conversão?").lower()
     price = float(input("Qual valor para conversão?"))
     validation(currency_quote, exchange, price)
+    teste = currency_converter(currency_quote, exchange)
+    response = teste(price)
+    search_conversion(response)
     
 
 client()
 
-# requisição get para site de cotação ainda em desenvolvimento
-def currency_converter(origin_currency, destination_currency):
-    def get_value(price):
-        link = f"https://wise.com/br/currency-converter/{currency_quote}-to-{exchange}-rate?amount={price}"
-        return link
-    return get_value
 
-#configurar o headers para 
-headers ={"user-agent" : "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Mobile Safari/537.36"}
+# Função para separar atividades no caso do BeatifulSoup para deixar o webscrapper mais organizado e buscar o valor da cotação do site.
 
-requisicao = requests.get(link, headers=headers)
-
-site = BeautifulSoup(requisicao.text, "html.parser")
-print(site.prettify())
-
-value = site.find('input', id="target-input")
-print(value.get('value'))
-
+# Só que podemos aplicar reduce com lambda e trocar find para conseguir atender as atividades propostas 
+def search_conversion(response):
+    site = BeautifulSoup(response.text, "html.parser")
+    value = site.find('input', id="target-input")
+    print(value.get('value'))
